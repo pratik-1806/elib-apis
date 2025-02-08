0015ce3f72971a44eb5de61545223c4f36a31856
Pratik Pansare <ppratikpansare98@gmail.com>
2025-02-08 18:28:23 +0530

    error handling for creating user and formatting

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,42 +5,47 @@ import userModel from "./userModel";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
 
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
 
-const createUser =async (req: Request,res: Response, next: NextFunction)=>{
-    
-    const {name, email, password} = req.body;
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
 
-    if(!name || !email || !password){
-        const error = createHttpError(400, "All fields are required");
-        return next(error);
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (user) {
+      const error = createHttpError(400, "User already exists");
+      next(error);
     }
+  } catch (err) {
+    return next(createHttpError(500, `Error while getting user: ${err}`));
+  }
 
-    const user = await userModel.findOne({email})
+  //hashed password
 
-    if(user){
-        const error = createHttpError(400, "User already exists");
-        next(error);
-    }
-
-    //hashed password
-
-    const hashedPassword =await bcrypt.hash(password, 10);
-    console.log(hashedPassword)
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
 
     const newUser = await userModel.create({
-        name,
-        email,
-        password:hashedPassword
-    }) 
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     //token generation
 
-    const token = sign({sub:newUser._id}, config.secretKey as string, {expiresIn : "7d"})
+    const token = sign({ sub: newUser._id }, config.secretKey as string, {
+      expiresIn: "7d",
+    });
 
+    res.json({ accessToken: token });
+  } catch (err) {
+    return next(createHttpError(400, `Error while creating user ${err}`));
+  }
+};
 
-   res.json({accessToken : token})
-}
-
-
-
-export {createUser};
+export { createUser };
