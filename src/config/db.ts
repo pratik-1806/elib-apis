c2978d8935d87a50b786d0107778fa6ed15349ee
Pratik Pansare <ppratikpansare98@gmail.com>
2025-02-09 01:55:25 +0530

    database configuration

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,7 +7,7 @@ const connectDB = async()=>{
     mongoose.connect(config.databaseUrl as string).then(()=>{
         console.log("Connected to DB")
     }).catch((err)=>{
-        console.log(`error while connecting ${err}`)
+        console.log(`error while connecting= ${err.message}`)
         process.exit(1);
     })
 }
