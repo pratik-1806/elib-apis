c31558afacf155c1bd56bc634eabee05f03f2534
Pratik Pansare <ppratikpansare98@gmail.com>
2025-02-09 10:20:53 +0530

    for checking

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,6 +3,7 @@ import { config } from "./config";
 
 
 const connectDB = async()=>{
+    console.log(config.databaseUrl)
 
     mongoose.connect(config.databaseUrl as string).then(()=>{
         console.log("Connected to DB")
